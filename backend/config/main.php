ddf01da3250e16da708ccc52c84b4af73adfbf39
gallih <gallearmadaw@gmail.com>
2021-04-27 20:24:30 +0700

    DEVELOPMENT BACKEND

--- backend/config/main.php
+++ backend/config/main.php
@@ -47,7 +47,9 @@ return [
         'view' => [
             'theme' => [
                 'pathMap' => [
-                    '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app'
+                    // '@app/views' => '@vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app',
+                    '@app/views' => 'yiister\gentelella\assets\Asset',
+                    
                 ]
             ],
         ],
--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -9,22 +9,24 @@ use yii\bootstrap\ActiveForm;
 
 $this->title = 'Login';
 ?>
-<div class="site-login">
+  <div class="login_wrapper">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>Please fill out the following fields to login:</p>
-
+    
+    <div class="animate form login_form">
+        <section class="login_content">
+            <div>
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
                 <div class="form-group">
                     <?= Html::submitButton('Login', ['class' => 'btn btn-primary btn-block', 'name' => 'login-button']) ?>
                 </div>
-
             <?php ActiveForm::end(); ?>
+            </div>
+        </section>                
 </div>
